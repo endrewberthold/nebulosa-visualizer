8ced98678fab4e2e4bbf3b850031ef1a649ae583
Endrew Berthold <endrew.berthold@gmail.com>
2025-12-03 00:09:31 -0300

    Add Spotify configuration structure

---
+++ src/config/spotifyConfig.js
@@ -0,0 +1,22 @@
+/**
+ * Configuration for Spotify API Integration.
+ */
+
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI
+
+const scopes = [
+    'streaming',
+    'user-read-email',
+    'user-read-private',
+    'user-read-playback-state',
+    'user-modify-playback-state'
+]
+
+export const spotifyConfig = {
+    clientId,
+    redirectUri,
+    scopes,
+    // Helper to generate the login URL
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+}

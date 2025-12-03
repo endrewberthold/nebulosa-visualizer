fb6b41c73544a9b697312eb0e6323a91a4a8c462
Endrew Berthold <endrew.berthold@gmail.com>
2025-12-02 23:40:50 -0300

    Initial commit: Setup React with Tailwind 4

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+function App() {
+  const [isReady, setIsReady] = useState(false)
+
+  const handleStartApplication = () => {
+      setIsReady(true)
+      console.log('Application Stated')
+  }
+
+  return (
+    <div
+    id="ApplicatioContainer"
+    className="w-full h-screen bg-neutral-900 text-white flex flex-col items-center justify-center"
+    >
+        <h1 className="text-4xl font-bold mb-4">
+            Nebulosa Audio Visualizer
+        </h1>
+
+        <button
+            onClick={handleStartApplication}
+            className="px-6 py-2 bg-green-500 hover:bg-green-600 rounded-full font-semibold transition-colors cursor-pointer"
+        >
+            {isReady ? 'System Ready' : 'Initialize App'}
+        </button>
+    </div>
+  )
+}
+
+export default App
